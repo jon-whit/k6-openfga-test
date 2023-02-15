ce6768fe791cb7b2bd8c0f258fed33b7024de38d
Jonathan Whitaker <jon.whitaker@auth0.com>
2023-02-15 14:09:24 -0700

    chore: add `API_TOKEN` env variable

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 import { random, chunk } from 'lodash';
 
 let API_BASE_URI = __ENV.API_BASE_URI
 if (!API_BASE_URI) {
   API_BASE_URI = "http://localhost:8080"
+}
+
+let headers = { 'Content-Type': 'application/json' };
+
+const API_TOKEN = __ENV.API_TOKEN
+if (API_TOKEN) {
+  headers['Authorization'] = `Bearer ${API_TOKEN}`;
 }
 
 const STORE_ID = __ENV.STORE_ID
@@ -32,32 +39,70 @@ if (!TOTAL_ORGS) {
     TOTAL_ORGS = 35;
 }
 
-const tuples = new Set();
-const checks = new Set();
+export const options = {
+  setupTimeout: '10m',
+  scenarios: {}
+}
 
-const users = [];
-const orgs = {};
-const publicRepos = [];  // 20%
-const privateRepos = []; // 40% direct association with random users
-const groupRepos = [];   // 40% group association with groups
+let scenarios = {
+  constant_rps: {
+    executor: 'constant-arrival-rate',
+    rate: 250,
+    timeUnit: '1s',
+    duration: '1h',
+    preAllocatedVUs: 20,
+    maxVUs: 40,
+  },
+  constant_rps_with_ramp: {
+    executor: 'ramping-arrival-rate',
+    startRate: 50,
+    timeUnit: '1s',
+    preAllocatedVUs: 20,
+    maxVUs: 40,
+    stages: [
+      { target: 50, duration: '1m' },
+      { target: 100, duration: '1m' },
+      { target: 250, duration: '2m' },
+      { target: 250, duration: '10m' },
+      { target: 0, duration: '1m' },
+    ],
+  }
+};
 
-for (let i = 0; i < TOTAL_USERS; i++) {
+if (__ENV.scenario) {
+  // use single scenario if `--env scenario=<example>` is provided
+  options.scenarios[__ENV.scenario] = scenarios[__ENV.scenario];
+} else {
+  options.scenarios = scenarios;
+}
+
+function bootstrap() {
+  const tuples = new Set();
+  const checks = new Set();
+
+  const users = [];
+  const orgs = {};
+  const publicRepos = [];  // 20%
+  const privateRepos = []; // 40% direct association with random users
+  const groupRepos = [];   // 40% group association with groups
+
+  for (let i = 0; i < TOTAL_USERS; i++) {
     users.push(`user:${i.toString()}`);
-}
-  
-for (let i = 0; i < TOTAL_REPOS * .20; i++) {
+  }
+
+  for (let i = 0; i < TOTAL_REPOS * .20; i++) {
     publicRepos.push(i.toString());
-}
+  }
 
-for (let i = 0; i < TOTAL_REPOS * .40; i++) {
-    privateRepos.push(i.toString());
-}
+  for (let i = 0; i < TOTAL_REPOS * .40; i++) {
+      privateRepos.push(i.toString());
+  }
 
-for (let i = 0; i < TOTAL_REPOS * .40; i++) {
+  for (let i = 0; i < TOTAL_REPOS * .40; i++) {
     groupRepos.push(i.toString());
   }
 
-for (let i = 0; i < publicRepos.length; i++) {
+  for (let i = 0; i < publicRepos.length; i++) {
     const repo = `repo:${publicRepos[i]}`;
   
     tuples.add({ object: repo , relation: 'reader', user: 'user:*' });
@@ -65,16 +110,16 @@ for (let i = 0; i < publicRepos.length; i++) {
     for (let j = 0; j < 5; j++) {
       checks.add({ object: repo, relation: 'reader', user: users[random(0, TOTAL_USERS-1)] });
     }
-}
+  }
 
-for (let i = 0; i < privateRepos.length; i++) {
+  for (let i = 0; i < privateRepos.length; i++) {
     const user = users[random(0, TOTAL_USERS-1)];
   
     tuples.add({ object: `repo:${privateRepos[i]}`, relation: 'writer', user: user });
     checks.add({ object: `repo:${privateRepos[i]}`, relation: 'reader', user: user });
-}
+  }
 
-for (let i = 0; i < TOTAL_ORGS; i++) {
+  for (let i = 0; i < TOTAL_ORGS; i++) {
     const orgName = i.toString();
     orgs[orgName] = { members: [], teams: { security: [] }, repos: [] };
 
@@ -107,76 +152,55 @@ for (let i = 0; i < TOTAL_ORGS; i++) {
     orgs[orgName].teams['security'] = [...new Set(orgs[orgName].teams['security'])];
     orgs[orgName].members = [...new Set(orgs[orgName].members)];
     orgs[orgName].repos = [...new Set(orgs[orgName].repos)];
-}
+  }
   
-  // org tuples
-Object.keys(orgs).forEach((orgName) => {
-    const org = orgs[orgName];
-    // security team
-    org.repos.forEach((repo) => {
-      tuples.add({ user: `org:${orgName}`, relation: 'owner', object: `repo:${orgName}/${repo}`});
-    });
-
-    tuples.add({ user: `team:${orgName}/security#member`, relation: 'repo_admin', object: `org:${orgName}`});
-
-    Object.keys(org.teams).forEach((team) => {
-      let assignedRepos = [];
-      if (team !== 'security') {
-        const reposToAssign = random(0, org.repos.length);
-        for (let i = 0; i < reposToAssign; i++) {
-          const repo = `repo:${orgName}/${org.repos[i]}`;
-          assignedRepos.push(repo);
-        }
-        assignedRepos = [...new Set(assignedRepos)];
-        assignedRepos.forEach((repo) => {
-          tuples.add({
-            user: `team:${orgName}/${team}#member`,
-            relation: 'admin',
-            object: repo
-          });
-        });
-      }
-      org.teams[team].forEach((member) => {
-        tuples.add({ user: member, relation: 'member', object: `team:${orgName}/${team}`});
-        if (team === 'security') {
-          org.repos.forEach((repo) => {
-            checks.add({user: member, relation: 'admin', object: `repo:${orgName}/${repo}`});
-          });
-        } else {
-          assignedRepos.forEach((repo) => {
-            checks.add({user: member, relation: 'writer', object: repo});
-          });
-        }
+  Object.keys(orgs).forEach((orgName) => {
+      const org = orgs[orgName];
+      // security team
+      org.repos.forEach((repo) => {
+        tuples.add({ user: `org:${orgName}`, relation: 'owner', object: `repo:${orgName}/${repo}`});
       });
-    });
-});
 
-export const options = {
-  scenarios: {}
-}
+      tuples.add({ user: `team:${orgName}/security#member`, relation: 'repo_admin', object: `org:${orgName}`});
 
-let scenarios = {
-  constant_rps: {
-    executor: 'constant-arrival-rate',
-    rate: 1500,
-    timeUnit: '1s',
-    duration: '1m',
-    preAllocatedVUs: 20,
-    maxVUs: 40
-  },
-};
+      Object.keys(org.teams).forEach((team) => {
+        let assignedRepos = [];
+        if (team !== 'security') {
+          const reposToAssign = random(0, org.repos.length);
+          for (let i = 0; i < reposToAssign; i++) {
+            const repo = `repo:${orgName}/${org.repos[i]}`;
+            assignedRepos.push(repo);
+          }
+          assignedRepos = [...new Set(assignedRepos)];
+          assignedRepos.forEach((repo) => {
+            tuples.add({
+              user: `team:${orgName}/${team}#member`,
+              relation: 'admin',
+              object: repo
+            });
+          });
+        }
+        org.teams[team].forEach((member) => {
+          tuples.add({ user: member, relation: 'member', object: `team:${orgName}/${team}`});
+          if (team === 'security') {
+            org.repos.forEach((repo) => {
+              checks.add({user: member, relation: 'admin', object: `repo:${orgName}/${repo}`});
+            });
+          } else {
+            assignedRepos.forEach((repo) => {
+              checks.add({user: member, relation: 'writer', object: repo});
+            });
+          }
+        });
+      });
+  });
 
-const headers = { 'Content-Type': 'application/json' };
-
-
-if (__ENV.scenario) {
-  // ise single scenario if `--env scenario=<example>` is used
-  options.scenarios[__ENV.scenario] = scenarios[__ENV.scenario];
-} else {
-  options.scenarios = scenarios;
+  return {tuples, checks};
 }
 
 export function setup() {
+
+  const {tuples, checks} = bootstrap();
 
   let res = http.post(`${API_BASE_URI}/stores/${STORE_ID}/authorization-models`, JSON.stringify({
     "type_definitions": [
@@ -414,31 +438,30 @@ export function setup() {
 
   const batches = chunk(Array.from(tuples), TUPLES_PER_WRITE);
 
-
-  let requests = []
   for (let i = 0; i < batches.length; i++) {
-    
-    requests.push(['POST', `${API_BASE_URI}/stores/${STORE_ID}/write`, JSON.stringify({
+    let payload = JSON.stringify({
       "writes": {
           "tuple_keys": batches[i]
       }
-    }), null])
-  }
+    })
 
-  let responses = http.batch(requests);
+    let resp = http.post(`${API_BASE_URI}/stores/${STORE_ID}/write`, payload), {headers: headers});
 
-  responses.forEach((res) => {
-    if (res.status !== 200) {
-      console.log(`failed to write tuples: received status '${res.status}'`);
-      console.log(`response body: '${res.body}'`)
-    }
-
-    check(res, {
+    check(resp, {
       "write response was 200": (r) => r.status === 200,
     });
-  });
 
-  return { checks, modelId };
+    if (resp.error) {
+      console.log(`write failure: ${resp.body}`);
+    }
+
+    if (resp.headers["x-ratelimit-remaining"] === 1) {
+      console.log("rate limit reached, throttling outgoing write traffic");
+      sleep(1);
+    }
+  }
+
+  return { tuples, checks, modelId};
 }
 
 export default function (data) {
@@ -458,5 +481,29 @@ export default function (data) {
   })
 }
 
-export function teardown() {
+export function teardown(data) {
+  const tuples = data.tuples;
+
+  const batches = chunk(Array.from(tuples), TUPLES_PER_WRITE);
+
+  for (let i = 0; i < batches.length; i++) {
+    let resp = http.post(`${API_BASE_URI}/stores/${STORE_ID}/write`, JSON.stringify({
+      "deletes": {
+          "tuple_keys": batches[i]
+      }
+    }), {headers: headers});
+
+    check(resp, {
+      "delete response was 200": (r) => r.status === 200,
+    });
+
+    if (resp.error) {
+      console.log(`delete failure: ${resp.body}`);
+    }
+
+    if (resp.headers["x-ratelimit-remaining"] === 1) {
+      console.log("rate limit reached, throttling outgoing write traffic");
+      sleep(1);
+    }
+  }
 }
